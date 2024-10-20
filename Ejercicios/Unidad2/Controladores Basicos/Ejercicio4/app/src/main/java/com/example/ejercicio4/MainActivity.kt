8a62c5202a00ccb271f589198d3703c41e9ba7d6
jasondgian@github.com <davjasg@gmail.com>
2024-10-20 15:48:25 +0200

    Ejercicio 4 pruebas

--- Ejercicios/Unidad2/Controladores Basicos/Ejercicio4/app/src/main/java/com/example/ejercicio4/MainActivity.kt
+++ Ejercicios/Unidad2/Controladores Basicos/Ejercicio4/app/src/main/java/com/example/ejercicio4/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.ejercicio4
 
 import android.os.Bundle
+import android.util.Log
+import android.view.DragEvent
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,80 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Almacena la vista en una variable.
+        val texto1 = findViewById<TextView>(R.id.tvTexto1)
+
+        // "Setea el escuchador
+        texto1.setOnDragListener { thisView, dragEvent ->
+            when(dragEvent.action) {
+
+                DragEvent.ACTION_DRAG_STARTED -> {
+                    Log.d("Funcion drag", "DRAG INICIADO")
+                    true
+                }
+
+                DragEvent.ACTION_DRAG_ENTERED ->{
+                    Log.d("Funcion drag", "DRAG ENTRADO")
+                    true
+                }
+                DragEvent.ACTION_DRAG_EXITED ->{
+                    Log.d("Funcion drag", "DRAG SALIDA")
+                    true
+                }
+                else -> false // Return false for unhandled actions
+
+            }
+
+
+
+
+        }
+
+
+        // Busca la vista.
+        val texto2 = findViewById<TextView>(R.id.tvTexto2)
+
+        // Setea el "escuchador"
+        texto2.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                texto2.text = "Tiene focus."  // Correctly setting the text
+                Log.d("FUNCION FOCUS", "ESTA EN FOCO")
+            } else {
+                texto2.text = "No tiene focus" // Correctly setting the text
+                Log.d("FUNCION FOCUS", "NO ESTA EN FOCO")
+            }
+        }
+
+
+
+        // Busca la vista.
+        val texto3 = findViewById<TextView>( R.id.tvTexto3 )
+
+        texto3.setOnKeyListener{ view , keyCode, event ->
+
+            if ( event.action == KeyEvent.ACTION_UP ){  // Esto no hace lo que yo creia. Ver mejor.
+                Log.d("Funcion onKey", "ACTION UP")
+                    true
+            }else {
+                false
+            }
+
+
+        }
+
+        val texto4 = findViewById<TextView>(R.id.tvTexto4)
+
+        texto4.setOnTouchListener { view, motionEvent ->
+            if ( motionEvent.action == MotionEvent.ACTION_BUTTON_PRESS ){
+                Log.d("Funcion Touch", "Toque detectado")
+                true
+            }
+            else {false}
+        }
+
+
+
     }
+
 }
